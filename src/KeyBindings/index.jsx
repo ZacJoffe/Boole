044c5cb4f1a384e53709aeec23cc168fbeb6a343
Zac Joffe <zacharyjoffe@gmail.com>
2019-10-08 11:40:20 -0400

    Fixed drop down

--- src/KeyBindings/index.jsx
+++ src/KeyBindings/index.jsx
@@ -5,7 +5,7 @@ import "./styles.css";
 export default class KeyBindings extends Component {
   render() {
     return (
-      <select value={this.props.binding}>
+      <select onChange={this.props.binding}>
         <option value="">Normal</option>
         <option value="vim">Vim</option>
         <option value="emacs">Emacs</option>
